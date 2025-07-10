30f2f753158adeb790651b65e954c08ca3159d1a
Dominik Janiga <dominikjaniga91@gmail.com>
2025-07-10 13:39:35 +0200

    SDI-11: Use type safe config

--- 08-url-shortener/src/main/java/url/shortener/AppConfig.java
+++ 08-url-shortener/src/main/java/url/shortener/AppConfig.java
@@ -1,66 +1,44 @@
 package url.shortener;
 
-import org.yaml.snakeyaml.Yaml;
+import com.typesafe.config.Config;
 
-import java.io.IOException;
-import java.util.Map;
-
-import static java.lang.String.format;
+import static com.typesafe.config.ConfigFactory.defaultOverrides;
+import static com.typesafe.config.ConfigFactory.parseResourcesAnySyntax;
 
 class AppConfig {
 
-    private final DatabaseConfig database;
-    private final JedisConfig jedis;
+    private final Config config;
 
-    private AppConfig(DatabaseConfig database, JedisConfig jedis) {
-        this.database = database;
-        this.jedis = jedis;
+    private AppConfig(Config config) {
+        this.config = config;
     }
 
     public static AppConfig load() {
-        String profile = System.getProperty("profile");
-        Yaml yaml = new Yaml();
-        try (var inputStream = AppConfig.class.getClassLoader().getResourceAsStream(format("configuration-%s.yaml", profile))) {
-            if (inputStream == null) {
-                throw new RuntimeException("Missing configuration.yaml in resources");
-            }
+        String configFileName = String.format("%s.conf", System.getProperty("profile"));
+        Config config = defaultOverrides()
+                .withFallback(parseResourcesAnySyntax(configFileName))
+                .resolve();
 
-            Map<String, Object> config = yaml.load(inputStream);
-            return new AppConfig(
-                    new DatabaseConfig((Map<String, Object>) config.get("database")),
-                    new JedisConfig((Map<String, Object>) config.get("jedis"))
-            );
-
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to load configuration", e);
-        }
+        return new AppConfig(config);
     }
 
-    String getUsername() { return database.username; }
-    String getPassword() { return database.password; }
-    String getUrl() { return database.url; }
-    String getJedisHost() { return jedis.host; }
-    int getJedisPort() { return jedis.port; }
-
-    private static class DatabaseConfig {
-        private final String username;
-        private final String password;
-        private final String url;
-
-        private DatabaseConfig(Map<String ,Object> config) {
-            username = config.get("username").toString();
-            password = config.get("password").toString();
-            url = config.get("url").toString();;
-        }
+    String getUsername() {
+        return config.getString("database.username");
     }
 
-    private static class JedisConfig {
-        private final String host;
-        private final int port;
+    String getPassword() {
+        return config.getString("database.password");
+    }
 
-        private JedisConfig(Map<String ,Object> config) {
-            this.host = config.get("host").toString();
-            this.port = Integer.parseInt(config.get("port").toString());
-        }
+    String getUrl() {
+        return config.getString("database.url");
+    }
+
+    String getJedisHost() {
+        return config.getString("jedis.host");
+    }
+
+    int getJedisPort() {
+        return config.getInt("jedis.port");
     }
 }
--- api-gateway/src/main/java/gateway/AppConfig.java
+++ api-gateway/src/main/java/gateway/AppConfig.java
@@ -1,15 +1,9 @@
 package gateway;
 
 import com.typesafe.config.Config;
-import com.typesafe.config.ConfigFactory;
-import org.yaml.snakeyaml.Yaml;
-
-import java.io.IOException;
-import java.util.Map;
 
 import static com.typesafe.config.ConfigFactory.defaultOverrides;
 import static com.typesafe.config.ConfigFactory.parseResourcesAnySyntax;
-import static java.lang.String.format;
 
 class AppConfig {
 
